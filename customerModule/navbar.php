7b5fd527619febddfa9e7ff2a2ddef2ccd8a48e2
stefocc <78801064+stefocc@users.noreply.github.com>
2021-03-09 21:50:57 +0200

    new

---
+++ customerModule/navbar.php
@@ -0,0 +1,10 @@
+    <!--Navbar-->
+    <div class="sidenav" id = "mySidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="manageClasses.php">Classes</a>
+        <a href="contactQueries.php">Queries</a>
+        <a href="../includes/logout.inc.php" class= "logoutbtn">Logout</a>
+    </div>
+
+    <!--Open navbar-->
+    <span style=" color: white;font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
