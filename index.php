7b253f1b014bdd072441fb3c91868d129722c8bf
Christophoros Prodromou <52533416+Chrisprod06@users.noreply.github.com>
2021-03-09 21:59:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
 </head>
 
 <body>
-<<<<<<< HEAD
     
 <!--Header-->    
 <div class="header">
@@ -21,26 +20,6 @@
     
 </div>
 <hr class="border">   
-
-<!--Menu-->
-
-<div class="menu-row">
-    <div class="menu-column"style="width:33.33%">      
-            <a href="adminModule/loginAdmin.php" class="menu-button">Admin</a>         
-    </div>
-    <div class="menu-column"style="width:33.33%">   
-            <a href="customerModule/loginCustomer.php" class="menu-button">Customer</a>  
-=======
-
-    <!--Header-->
-    <div class="header">
-        <h1>Welcome to ELIT3 fitness center!</h1>
-        <h2>Select your role to continue.</h2>
-
->>>>>>> 53bd161596a865e9aad4f09139ce7080060f88d1
-    </div>
-    <hr class="border">
-
     <!--Menu-->
 
     <div class="menu-row">
